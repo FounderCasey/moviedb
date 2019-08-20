e2538007b312d91b75b8800d74b75e8fb047e0a5
FounderCasey <caseywilcox@outlook.com>
2019-08-20 07:54:39 -0700

    Initial Commit

---
+++ Movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Coco and Cream on 8/20/19.
+//  Copyright © 2019 Coco and Cream. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
